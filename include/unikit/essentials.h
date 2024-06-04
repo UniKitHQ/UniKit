a201e150a32464160dae7676881d17c4fe988a1c
Heewon Cho <heewon6752@gmail.com>
2024-06-05 01:58:12 +0900

    Change spdx identifier license

--- include/unikit/essentials.h
+++ include/unikit/essentials.h
@@ -1,6 +1,6 @@
-/* SPDX-License-Identifier: MPL-2.0 */
+/* SPDX-License-Identifier: Elastic-2.0 */
 /* Copyright (c) 2023, Pribess (Heewon Cho).
- * Licensed under the MPL-2.0 License.
+ * Licensed under the Elastic-2.0 License.
  * You may not use this file except in compliance with the License.
  */
 
