b1e55c5ab56645ee72bd3da6d7fc0ffb6e34bf91
Heewon Cho <heewon6752@gmail.com>
2024-08-18 01:45:06 +0900

    Add nostd header file

---
+++ include/unikit/plat/nostd.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: Elastic-2.0 */
+/* Copyright (c) 2024, Pribess (Heewon Cho).
+ * Licensed under the Elastic-2.0 License.
+ * You may not use this file except in compliance with the License.
+ */
+
+#ifndef __UNIKIT_PLAT_NOSTD_H__
+#define __UNIKIT_PLAT_NOSTD_H__
+
+#include <unikit/essentials.h>
+
+
+void *memcpy(void *dst, const void *src, sz len);
+void *memmove(void *dst, const void *src, sz len);
+void *memset(void *dst, int value, sz len);
+int memcmp(const void *s1, const void *s2, sz len);
+
+#endif /* __UNIKIT_PLAT_NOSTD_H__ */
