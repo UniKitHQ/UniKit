a04b7afc14811eb5c59e502ab740a83c0f289aab
Heewon Cho <heewon6752@gmail.com>
2025-09-21 00:11:38 +0900

    Add restructured page map prototype

--- core/arch/x86_64/memory/paging.zig
+++ core/arch/x86_64/memory/paging.zig
@@ -7,7 +7,7 @@
 
 pub const PageTable = struct {
     pub fn create(T: type, entries: []const T) [0x200]T {
-        if (T != ReferencePageEntry and T != PDPTE_1GB and T != PDE_2MB and T != PTE_4KB)
+        if (T != TableEntry and T != PDPTE_1GB and T != PDE_2MB and T != PTE_4KB)
             @compileError("The type must be one of the page table entry types");
 
         if (entries.len > 0x200)
@@ -17,7 +17,7 @@ pub const PageTable = struct {
     }
 
     pub fn fill(T: type, entry: T, count: usize) [count]T {
-        if (T != ReferencePageEntry and T != PDPTE_1GB and T != PDE_2MB and T != PTE_4KB)
+        if (T != TableEntry and T != PDPTE_1GB and T != PDE_2MB and T != PTE_4KB)
             @compileError("The type must be one of the page table entry types");
 
         if (count > 0x200)
@@ -31,38 +31,7 @@ pub const PageTable = struct {
     }
 };
 
-const ReferencePageEntry = packed struct(u64) {
-    /// Present
-    p: bool = true,
-    /// Read/Write
-    rw: enum(u1) {
-        readonly = 0,
-        writeable = 1,
-    } = .readonly,
-    /// User/Supervisor
-    us: enum(u1) {
-        supervisor = 0,
-        user = 1,
-    } = .supervisor,
-    /// Page-level write-through
-    pwt: bool = false,
-    /// Page-level cache disable
-    pcd: bool = false,
-    /// Accessed
-    a: bool = false,
-    _0: u5 = 0x00,
-    /// HLAT Ignore
-    r: bool = false,
-    /// Table Address
-    address: u40,
-    _1: u11 = 0x00,
-    /// Execute Disable
-    xd: bool = false,
-};
-
-pub const PML4E = ReferencePageEntry;
-
-pub const PDPTE = ReferencePageEntry;
+pub const PML4E = TableEntry;
 
 pub const PDPTE_1GB = packed struct(u64) {
     /// Present
@@ -104,7 +73,7 @@ pub const PDPTE_1GB = packed struct(u64) {
     xd: bool = false,
 };
 
-pub const PDE = ReferencePageEntry;
+pub const PDE = TableEntry;
 
 pub const PDE_2MB = packed struct(u64) {
     /// Present
@@ -182,3 +151,167 @@ pub const PTE_4KB = packed struct(u64) {
     /// Execute Disable
     xd: bool = false,
 };
+
+pub const Page = struct {
+    pub const Table = struct {
+        pub const Entry = PageEntry;
+    };
+
+    pub const Directory = struct {
+        pub const Entry = packed union {
+            table: TableEntry,
+            page: HugePageEntry(21),
+        };
+    };
+
+    pub const DirectoryPointer = struct {
+        pub const Entry = packed union {
+            table: TableEntry,
+            page: HugePageEntry(30),
+        };
+    };
+
+    pub const MapLevel4 = struct {
+        pub const Entry = TableEntry;
+    };
+
+    pub const MapLevel5 = struct {
+        pub const Entry = TableEntry;
+    };
+};
+
+const TableEntry = packed struct(u64) {
+    /// Present
+    p: bool = true,
+    /// Read/Write
+    rw: enum(u1) {
+        readonly = 0,
+        writeable = 1,
+    } = .readonly,
+    /// User/Supervisor
+    us: enum(u1) {
+        supervisor = 0,
+        user = 1,
+    } = .supervisor,
+    /// Page-level write-through
+    pwt: bool = false,
+    /// Page-level cache disable
+    pcd: bool = false,
+    /// Accessed
+    a: bool = false,
+    _0: u1 = 0x00,
+    /// Page Size
+    ps: bool = false,
+    _1: u3 = 0x00,
+    /// HLAT Ignore
+    r: bool = false,
+    /// Table Address
+    address: u40,
+    _2: u11 = 0x00,
+    /// Execute Disable
+    xd: bool = false,
+
+    pub fn create(address: u64, entry: @This()) @This() {
+        entry.address = address >> 12;
+        return entry;
+    }
+
+    pub fn getAddress(self: @This()) u64 {
+        return self.address << 12;
+    }
+};
+
+const PageEntry = packed struct(u64) {
+    /// Present
+    p: bool = true,
+    /// Read/Write
+    rw: enum(u1) {
+        readonly = 0,
+        writeable = 1,
+    } = .readonly,
+    /// User/Supervisor
+    us: enum(u1) {
+        supervisor = 0,
+        user = 1,
+    } = .supervisor,
+    /// Page-level write-through
+    pwt: bool = false,
+    /// Page-level cache disable
+    pcd: bool = false,
+    /// Accessed
+    a: bool = false,
+    /// Dirty
+    d: bool = false,
+    /// Page Attribute Table
+    pat: bool = false,
+    /// Global
+    g: bool = false,
+    _0: u2 = 0x00,
+    /// HLAT Ignore
+    r: bool = false,
+    /// Page Frame Address
+    address: u40,
+    _1: u7 = 0x00,
+    /// Protection Key
+    pk: u4 = 0x00,
+    /// Execute Disable
+    xd: bool = false,
+
+    pub fn create(frame: u64, entry: @This()) @This() {
+        entry.address = frame << 12;
+        return entry;
+    }
+
+    pub fn getFrame(self: @This()) u64 {
+        return self.address >> 12;
+    }
+};
+
+fn HugePageEntry(comptime size: u64) type {
+    return packed struct(u64) {
+        /// Present
+        p: bool = true,
+        /// Read/Write
+        rw: enum(u1) {
+            readonly = 0,
+            writeable = 1,
+        } = .readonly,
+        /// User/Supervisor
+        us: enum(u1) {
+            supervisor = 0,
+            user = 1,
+        } = .supervisor,
+        /// Page-level write-through
+        pwt: bool = false,
+        /// Page-level cache disable
+        pcd: bool = false,
+        /// Accessed
+        a: bool = false,
+        /// Dirty
+        d: bool = false,
+        /// Page Size
+        ps: bool = true,
+        /// Global
+        g: bool = false,
+        _0: u2 = 0x00,
+        /// HLAT Ignore
+        r: bool = false,
+        /// Page Attribute Table
+        pat: bool = false,
+        address: u39,
+        _1: u7 = 0x00,
+        /// Protection Key
+        pk: u4 = 0x00,
+        /// Execute Disable
+        xd: bool = false,
+
+        pub fn create(frame: u64, entry: @This()) @This() {
+            entry.address = frame << size;
+            return entry;
+        }
+
+        pub fn getFrame(self: @This()) u64 {
+            return self.address >> size;
+        }
+    };
+}
